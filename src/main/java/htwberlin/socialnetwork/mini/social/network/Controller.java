5f54a10eade7331c0bb0e359dd316016ceda2814
dnlno <daniel-nothelfer@web.de>
2023-10-22 15:39:59 +0200

    hello world

---
+++ src/main/java/htwberlin/socialnetwork/mini/social/network/Controller.java
@@ -0,0 +1,13 @@
+package htwberlin.socialnetwork.mini.social.network;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
