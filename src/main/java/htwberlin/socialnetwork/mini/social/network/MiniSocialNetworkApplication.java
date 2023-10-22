f260cc87420ed477cbcb21555b2e73113ec8a62a
dnlno <daniel-nothelfer@web.de>
2023-10-22 15:18:27 +0200

    spring initializr

---
+++ src/main/java/htwberlin/socialnetwork/mini/social/network/MiniSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.socialnetwork.mini.social.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/socialnetwork/mini/social/network/MiniSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.socialnetwork.mini.social.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
